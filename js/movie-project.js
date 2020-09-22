2bff99d629482c4dc89962eeab425d4868ffe7d7
Michael Klanica <michael.david.klanica@gmail.com>
2020-09-22 13:39:14 -0500

    added posting and getting functionality

--- js/movie-project.js
+++ js/movie-project.js
@@ -3,9 +3,24 @@ $(document).ready(function() {
 
     const url = "https://fearless-famous-glass.glitch.me/movies";
 
+    // GET DATA FROM MOVIE API
+    fetch(url).then(response => response.json()).then(data => console.log(data));
 
-
-
+    // POST DATA TO MOVIE API
+    const movieRating = {
+        title: "Lost Highway",
+        rating: 5
+    };
+    const options = {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(movieRating),
+    };
+    fetch(url, options)
+        .then(response => console.log(response))
+        .catch(error => console.error(error));
 
 
 
