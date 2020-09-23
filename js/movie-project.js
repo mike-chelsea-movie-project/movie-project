ce883fdb494998c67b89bea3e2a441eb0939bd14
Michael Klanica <michael.david.klanica@gmail.com>
2020-09-23 18:55:40 -0500

    add-movie functionality works!

--- js/movie-project.js
+++ js/movie-project.js
@@ -43,7 +43,25 @@ $(document).ready(function () {
     // Add Screen Functionality
     $("#add-movie-btn").click(function(e){
         e.preventDefault();
-
+        let title = $('#movie-title-add').val();
+        let rating = $("input[name='movie-rating-add']:checked").val();
+        let genre = $("input[name='movie-genre']:checked").val();
+        // POST DATA TO MOVIE API
+        const movieRating = {
+            title: title,
+            rating: rating,
+            genre: genre
+        };
+        const options = {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(movieRating),
+        };
+        fetch(url, options)
+            .then(response => console.log(response))
+            .catch(error => console.error(error));
     })
 
     // Edit Screen Functionality --> Still working on this, we need to have it come up as popup and live/update/refresh the page
@@ -171,23 +189,3 @@ $(document).ready(function () {
     };
 
 });
-
-// POST DATA TO MOVIE API
-// const movieRating = {
-//     title: "Lost Highway",
-//     rating: 5
-// };
-// const options = {
-//     method: 'POST',
-//     headers: {
-//         'Content-Type': 'application/json',
-//     },
-//     body: JSON.stringify(movieRating),
-// };
-// fetch(url, options)
-//     .then(response => console.log(response))
-//     .catch(error => console.error(error));
-
-
-// GET RADIO BUTTON VALUE
-// var radioValue = $("input[name='gender']:checked").val();
